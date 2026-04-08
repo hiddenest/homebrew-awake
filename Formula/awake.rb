812d67deb6afd1e8da72fa72b2cd259119716c5e
Chanhee Lee <hiddenest12@gmail.com>
2026-04-08 23:40:01 +0900

    feat: add bottle block for v0.1.0

--- Formula/awake.rb
+++ Formula/awake.rb
@@ -3,7 +3,14 @@ class Awake < Formula
   homepage "https://github.com/hiddenest/awake"
   url "https://github.com/hiddenest/awake/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "38e26824d5db9e7ac40cfe62a790fc9037317ad018d9f21ee38d8a8592046e35"
+  license "MIT"
   head "https://github.com/hiddenest/awake.git", branch: "main"
+
+  bottle do
+    root_url "https://github.com/hiddenest/awake/releases/download/v0.1.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "97c5278ec67c105e10e022ba055cd31e98435216c91cfb94be07bdeda56903ac"
+  end
 
   depends_on "rust" => :build
   depends_on :macos
